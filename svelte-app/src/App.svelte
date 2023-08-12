9fdccc999f94fb12682748aa5fe5f53935271671
justinolcott <69178047+justinolcott@users.noreply.github.com>
2023-08-12 17:47:57 -0600

    update app

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -96,7 +96,7 @@
 
 
 <svelte:head>
-  <title>My Svelte App</title>
+  <title>Scripture Website</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- Other meta tags, CSS links, etc. -->
